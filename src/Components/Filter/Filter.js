507d0ae0a5d2d37e37589cedc44a489b0f7cd89e
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-08 23:27:08 +0200

    Update Filter.js

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 
+import s from './Filter.module.css';
+
 export default function Filter({ value, onChange }) {
   return (
-    <label>
+    <label className={s.label}>
       Find contacts by name
       <input type="text" value={value} onChange={onChange}></input>
     </label>
