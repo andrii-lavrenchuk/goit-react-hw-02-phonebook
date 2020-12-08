043ffb263fe1542d713e92a1ec2684bf02a8a93c
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-08 22:41:14 +0200

    Create Filter.js

---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export default function Filter({ value, onChange }) {
+  return (
+    <label>
+      Find contacts by name
+      <input type="text" value={value} onChange={onChange}></input>
+    </label>
+  );
+}
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
